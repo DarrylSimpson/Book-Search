3f26ad55e54571bbd4344c0c5de21b2615755852
DarrylSimpson <darryl.simpson0793@gmail.com>
2021-02-07 16:13:17 -0600

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection')
 const { authMiddleware } = require('./utils/auth')
+const mongoose = require('mongoose');
+
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -17,6 +19,15 @@ server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book-Search', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+
+module.exports = mongoose.connection;
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
